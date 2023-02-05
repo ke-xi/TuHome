8d90f27465240c23ebba43ff371ca34348a2f28e
ke-xi <1032663326@qq.com>
2023-02-05 16:04:49 +0800

    1

--- source/js/footer.js
+++ source/js/footer.js
@@ -1,7 +1,7 @@
 document.write('<!-----------  footer----------->');
 document.write('' +
     '<center>\n' +
-    '<p style="color:#333;">© 2021-2022 TuHome</p>\n' +
+    '<p style="color:#333;">© 2023-2023 KeXiYa</p>\n' +
     '</center>\n' +
     '');
 
