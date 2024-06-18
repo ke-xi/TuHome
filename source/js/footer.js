b3d7bdab1add6f6453441dc5c865473188815598
可惜公主爱王子 <1032663326@qq.com>
2024-06-18 22:18:10 +0800

    1

--- source/js/footer.js
+++ source/js/footer.js
@@ -1,10 +1,6 @@
 document.write('<!-----------  footer----------->');
 document.write('' +
     '<center>\n' +
-    '<p style="color:#333;">© 2023-2024 KeXiYa</p>\n' +
+    '<p style="color:#333;">© 2023-' + new Date().getFullYear() + ' KeXiYa</p>\n' +
     '</center>\n' +
     '');
-
-
-
-
