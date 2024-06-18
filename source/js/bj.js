a8709f68a7d18ffb2ebecaf3c98a248728bba33d
可惜公主爱王子 <1032663326@qq.com>
2024-06-18 22:49:37 +0800

    1

--- source/js/bj.js
+++
@@ -1,8 +0,0 @@
-var ua = navigator.userAgent;
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua)) {
-  // 如果UA中包含上述移动设备的关键字，认为是移动端
-  document.body.style.backgroundImage = "url('source\img\path-to-mobile-background.jpg')";
-} else {
-  // 否则认为是电脑端
-  document.body.style.backgroundImage = "url('source\img\path-to-desktop-background.jpg')";
-}
