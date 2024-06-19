b11009c58dee732c5d547dad4efecd41b79cd65e
可惜公主爱王子 <1032663326@qq.com>
2024-06-19 22:01:09 +0800

    更新 bj.js

--- source/js/bj.js
+++ source/js/bj.js
@@ -17,7 +17,7 @@ var deviceType = getDeviceType();
 
 // 根据设备类型设置背景图片
 if (deviceType === 'Android') {
-    document.body.style.backgroundImage = 'url(\'https://pic.yz001.online/images/2024/06/19/7mJJn.jpg\')';
+    document.body.style.backgroundImage = 'url(\'https://pic.yz001.online/images/2024/06/19/7m1MU.jpg\')';
 } else if (deviceType === 'iPhone/iPad') {
     document.body.style.backgroundImage = 'url(\'https://pic.yz001.online/images/2024/06/19/7m1MU.jpg\')';
 } else {
