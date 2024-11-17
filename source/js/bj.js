c2f12a45393fe43075b0f2cc7d5912d92754e6be
可惜公主爱王子 <k1032663326@gmail.com>
2024-11-17 16:22:13 +0800

    更新 bj.js

--- source/js/bj.js
+++ source/js/bj.js
@@ -17,9 +17,9 @@ var deviceType = getDeviceType();
 
 // 根据设备类型设置背景图片
 if (deviceType === 'Android') {
-    document.body.style.backgroundImage = 'url(\'https://pic.yz001.online/images/2024/06/19/7m1MU.jpg\')';
+    document.body.style.backgroundImage = 'url(\'https://i0.hdslb.com/bfs/article/a910d4335158f064ed5918898210608f44562710.jpg\')';
 } else if (deviceType === 'iPhone/iPad') {
-    document.body.style.backgroundImage = 'url(\'https://pic.yz001.online/images/2024/06/19/7m1MU.jpg\')';
+    document.body.style.backgroundImage = 'url(\'https://i0.hdslb.com/bfs/article/d823ebabcec12f8a145a821973ba714844562710.jpg\')';
 } else {
-    document.body.style.backgroundImage = 'url(\'https://pic.yz001.online/images/2024/06/19/70ybd.jpg\')';
+    document.body.style.backgroundImage = 'url(\'https://i0.hdslb.com/bfs/article/13db13cb5ea3d9a9c5f02c8f56c22cfd44562710.jpg\')';
 }
