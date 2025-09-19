f3cb04ae26cc3a9ac52a3d8f3d590934e28c1422
可惜公主爱王子 <58284266+ke-xi@users.noreply.github.com>
2025-09-19 21:15:15 +0800

    Update bj.js

--- source/js/bj.js
+++ source/js/bj.js
@@ -17,9 +17,9 @@ var deviceType = getDeviceType();
 
 // 根据设备类型设置背景图片
 if (deviceType === 'Android') {
-    document.body.style.backgroundImage = 'url(\'https://cdn.jsdelivr.net/gh/ke-xi/picx-images-hosting@master/アンノウン・マザーグース_123495003.58he9crdfp.jpg\')';
+    document.body.style.backgroundImage = 'url(\'http://file.daqsoft.com/uploadfile/server/geekui/niubi/The Finale of Summer_100784937.jpg\')';
 } else if (deviceType === 'iPhone/iPad') {
-    document.body.style.backgroundImage = 'url(\'https://cdn.jsdelivr.net/gh/ke-xi/picx-images-hosting@master/70日目-BANG!_123705735.8hgi60d6xx.png\')';
+    document.body.style.backgroundImage = 'url(\'https://3vj-vda.3vjia.com/meijia_oss_pro/95c2b0145b6c4926b8a828b22d2dce99_1300_1900.jpg\')';
 } else {
-    document.body.style.backgroundImage = 'url(\'https://cdn.jsdelivr.net/gh/ke-xi/picx-images-hosting@master/大脱走_124388672.9kg7gw648w.jpg\')';
+    document.body.style.backgroundImage = 'url(\'https://img20.360buyimg.com/openfeedback/jfs/t1/295588/32/22723/243271/68cd51e3F8dc5cea5/0737beef78ae5edd.jpg\')';
 }
